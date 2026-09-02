a93c0d3369816c18e0a294410ae5f98dcff5bb4c
bookteam7227 <bookteam7227@gmail.com>
2026-09-02 14:55:34 +0900

    API 따로

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// /firebase-config.js
+
+export const firebaseConfig = Object.freeze({
+    apiKey: "AIzaSyDz3dud_kVAA__WtNJAbPf7FVhhUFzDFkI",
+    authDomain: "dashboard-b9f24.firebaseapp.com",
+    projectId: "dashboard-b9f24",
+    storageBucket: "dashboard-b9f24.firebasestorage.app",
+    messagingSenderId: "664038459364",
+    appId: "1:664038459364:web:764352895560f0a17029f1"
+});
