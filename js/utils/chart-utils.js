6a0b016558ffe0b9cb68a909c6339e185f289b3a
bookteam7227 <bookteam7227@gmail.com>
2026-09-14 14:40:42 +0900

    Update chart-utils.js

--- js/utils/chart-utils.js
+++ js/utils/chart-utils.js
@@ -537,9 +537,23 @@ function renderAnnualComparisonTooltip(
         )
     );
 
+    const halfHeight =
+        (element.offsetHeight || 180) / 2;
+
+    const clampedTop =
+        Math.max(
+            halfHeight + 8,
+            Math.min(
+                top,
+                containerBox.height
+                - halfHeight
+                - 8
+            )
+        );
+
     element.style.opacity = 1;
     element.style.left = `${left}px`;
-    element.style.top = `${top}px`;
+    element.style.top = `${clampedTop}px`;
 }
 
 export function createAnnualChart(
